a7cfaaa48190aafe9b17562a82c118fdc8f44e55
Daria <daria.brazhnyk.suisse@gmail.com>
2016-11-05 07:33:30 +0100

    Menu

--- app/src/main/java/com/exemple/constrackerok/WelcomeActivity.java
+++ app/src/main/java/com/exemple/constrackerok/WelcomeActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.appindexing.Action;
@@ -38,6 +39,29 @@ public class WelcomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /*public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("WelcomeActivity", "onCreateOptionsMenu");
+
+        //Inflate the manu; this adds to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+
+  public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("WelcomeActivity", "onOptionsItemSelected");
+        //handle action bar clicks here
+//the action bar will automatically handle clicks on the home/Up button, so long
+        //as we specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemIf();
+
+        //nonInspection SimlifiableIfStatement
+        if (id == R.id.action_ssettings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 
     public void startSpeakerSpace(View view) {
         Intent intent = new Intent(this, LogInSpeakerSpace.class);
