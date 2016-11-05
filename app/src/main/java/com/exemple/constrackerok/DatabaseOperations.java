f52df2d54d5af2e0660960fd7735335475b1c80d
Daria <daria.brazhnyk.suisse@gmail.com>
2016-11-05 13:08:31 +0100

    DataBaseOperations,TableData, DataBaseHelper.

--- app/src/main/java/com/exemple/constrackerok/DatabaseOperations.java
+++ app/src/main/java/com/exemple/constrackerok/DatabaseOperations.java
@@ -3,19 +3,28 @@ package com.exemple.constrackerok;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
+import android.util.Log;
 
 
 public class DatabaseOperations extends SQLiteOpenHelper {
+public static final int database_version = 1;
+    // We create here a table
+    // using the table name
+    // we specify 2 column names and 2 data type (text)
+    // operator "," between 2 column names
+public String CREATE_QUERY =  "CREATE TABLE" + TableData.TableInfo.TABLE_NAME + "(" + TableData.TableInfo.USER_NAME + "TEXT, " + TableData.TableInfo.USER_PASSWORD + "TEXT );";
 
+    public DatabaseOperations(Context context){
 
-    public DatabaseOperations(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
-
-        super(context, name, factory, version);
+        super(context, TableData.TableInfo.DATABASE_NAME, null, database_version);
+   Log.d("Database operations", "Datase created");
     }
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+db.execSQL(CREATE_QUERY);
+        Log.d("Database operations", "Table created");
     }
 
     @Override
