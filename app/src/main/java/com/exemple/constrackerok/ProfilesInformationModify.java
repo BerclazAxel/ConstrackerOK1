c7722ec09b0214b781834355ff04a38ed2f32c19
Axel <ax.be666@hotmail.com>
2016-11-24 20:00:57 +0100

    ol

--- app/src/main/java/com/exemple/constrackerok/ProfilesInformationModify.java
+++ app/src/main/java/com/exemple/constrackerok/ProfilesInformationModify.java
@@ -21,8 +21,7 @@ public class ProfilesInformationModify extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+                        .setAction("Action", null).show();/*hziuzu*/            }
         });
     }
 
